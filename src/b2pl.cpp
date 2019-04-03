01a5f694b184d6e0ef963d6bb71b9f63b865a862
range3 <white.line.oar@gmail.com>
2019-04-03 17:13:14 +0900

    impl Basic 2PL

--- src/b2pl.cpp
+++ src/b2pl.cpp
@@ -1,6 +1,55 @@
+#include <cstdlib>
 #include <iostream>
-using namespace std;
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <array>
+#include <chrono>
+
+struct item {
+  int val;
+  std::mutex mutex;
+};
 
 int main(int argc, char* argv[]) {
-  cout << "Hello, World" << endl;
+
+  int nthreads = 2;
+  if(argc == 2) {
+    nthreads = std::atoi(argv[1]);
+  }
+
+  std::vector<std::thread> workers;
+  std::array<item, 100> items;
+
+  auto begin = std::chrono::high_resolution_clock::now();
+
+  for(int i = 0; i < nthreads; ++i) {
+    workers.emplace_back([=, &items](){
+      std::vector<size_t> tasks;
+      const int id = i;
+
+      tasks.push_back(id);
+      items[id].mutex.lock();
+      items[id].val = id;
+
+      tasks.push_back(id+1);
+      items[id+1].mutex.lock();
+      items[id+1].val = id;
+
+      for(auto task: tasks) {
+        items[task].mutex.unlock();
+      }
+    });
+  }
+
+  for(auto& worker : workers) {
+    worker.join();
+  }
+
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed_time = std::chrono::duration_cast<std::chrono::duration<double>>(end - begin);
+
+  std::cout << "Elapsed time (sec): " << elapsed_time.count() << std::endl;
+  std::cout << "Throuput (trans/sec): " << nthreads / elapsed_time.count() << std::endl;
+  return 0;
 }
